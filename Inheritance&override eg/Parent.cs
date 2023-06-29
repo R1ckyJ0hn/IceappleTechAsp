4ea1860922fa84bf3ae34dcc4377912a77b14161
R1ckyJ0hn <101105185+R1ckyJ0hn@users.noreply.github.com>
2023-06-29 21:58:40 +0530

    Add files via upload

---
+++ Inheritance&override eg/Parent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Parent
+    {
+        public void meth1()
+        {
+            Console.WriteLine("This is meth 1");
+        }
+        public void meth2()
+        {
+            Console.WriteLine("This is meth 2");
+        }
+        public void meth3()
+        {
+            Console.WriteLine("This is meth 3");
+        }
+
+        public virtual void meth4()
+        {
+            Console.WriteLine("This is meth 4");
+        }
+    }
+}
---
+++ Inheritance&override eg/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parent method =new Parent();
+            Child childmeth = new Child();
+            method.meth1();
+            method.meth2();
+            method.meth3();
+            method.meth4();
+            childmeth.meth1();
+            childmeth.meth2();
+            childmeth.meth3();
+            childmeth.meth4();
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
