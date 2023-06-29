4665662e83a87ccf9cefeac16c15a4245d2c19e3
R1ckyJ0hn <101105185+R1ckyJ0hn@users.noreply.github.com>
2023-06-29 14:57:20 +0530

    Add files via upload

---
+++ Getters and setters/Films .cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Films
+    {
+        public string name;
+        public string director;
+        private string rating;
+
+        public Films(string aname, string adirector, string arating)
+        {
+            name = aname;
+            director = adirector;
+            Rating = arating;
+        }
+
+        public string Rating
+        {
+            get { return rating; }
+
+            set
+            {
+                
+                if(value == "G" || value == "PG" || value == "PG-13" || value == "NR" ) {
+                    rating = value;
+                
+                }
+                else
+                {
+                    rating = "NR";
+                }
+            }
+        }
+    }
+}
---
+++ Getters and setters/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Films avengers = new Films("Avenger", "Joss wheadon", "PG-13");
+            Films Shrek = new Films("Shrek", "Rand", "Dog");
+            Console.ReadLine();
+        }
+
+
+    }
+}
